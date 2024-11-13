404150c88320ff0145211314ce1ca114e1ba060e
CaptainJurov <goudazv@gmail.com>
2024-11-14 02:32:57 +0700

    БОЛЬШЕ КРАСОТЫ

---
+++ colors.h
@@ -0,0 +1,15 @@
+#pragma once
+namespace Colors {
+    const char * red = "\e[1;31m";
+    const char * green = "\e[1;32m";
+    const char * yellow = "\e[1;33m";
+    const char * blue = "\e[1;34m";
+    const char * reset = "\e[0m";
+    const char * bold = "\e[1m";
+    const char * ital = "\e[3m";
+    const char * конец_ital = "\e[23m";
+    const char * зачёрк = "\e[21m";
+    const char * недозелёный = "\e[38;2;170;230;170m";
+    const char * скучный = "\e[38;2;130;130;130m";
+}
+//https://ru.wikipedia.org/wiki/%D0%A3%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D1%8F%D1%8E%D1%89%D0%B8%D0%B5_%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8_ANSI
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iterator>
 #include <algorithm>
 #include <forward_list>
+#include "colors.h"
 struct Машина {
     int id;
     Машина(const Машина& машина): id(машина.id) {} 
@@ -25,14 +26,14 @@ public:
     Стоянка() {}
     void вставить(Машина машина) {
         Машины.push_front(машина);
-        std::cout << "Автомобиль " << машина.id << " прибыл." << std::endl;
+        std::cout << Colors::green << "Автомобиль " << машина.id << " прибыл." << Colors::reset << std::endl;
     }
     auto GS() {
         return Машины;
     }
     void достать(Машина& машина) {
         if (Машины.empty()) {
-            std::cout<<"Стоянка пуста!\n";
+            std::cout<<Colors::red<<Colors::bold<<"Стоянка пуста!\n"<<Colors::reset;
             return;
         }
         std::forward_list<Машина> temp;
@@ -40,48 +41,54 @@ public:
         while (iter!=Машины.end() and (*iter) != машина) {
             iter++;
             if (iter==Машины.end()) {
-                std::cout << "Автомобиль " << машина << " не найден на стоянке." << std::endl;
+                std::cout << Colors::red << "Автомобиль " << Colors::ital << машина << Colors::конец_ital << " не найден на стоянке."<< Colors::reset << std::endl;
                 return;
             }
         }
+        std::cout<<Colors::скучный;
         temp.splice_after(temp.before_begin(), Машины, Машины.before_begin(), iter);
         std::copy(temp.begin(), temp.end(), std::ostream_iterator<Машина>(std::cout, " Временно выехал со стоянки\n"));
-        std::cout << "Автомобиль " << Машины.front() << " успешно уехал со стоянки." << std::endl;
+        std::cout << Colors::green << "Автомобиль " << Машины.front() << " успешно уехал со стоянки." << std::endl;
         Машины.pop_front();
         Машины.splice_after(Машины.before_begin(), temp);
         std::copy(temp.begin(), temp.end(), std::front_inserter(Машины));
+        std::cout<<Colors::reset;
     }
 
     void show() {
-        std::cout << "------\nМашины на стоянке:\n";
+        std::cout << Colors::недозелёный <<"Машины на стоянке:\n"<<Colors::ital;
         if (Машины.empty()) {
-            std::cout << "пусто" << std::endl;
+            std::cout << Colors::red <<"пусто"<<std::endl;
         } else {
             std::copy(Машины.begin(), Машины.end(), std::ostream_iterator<Машина>(std::cout, "\n"));
-            std::cout<<"------\n";
         }
+        std::cout<<Colors::reset;
     }
 };
 
-int main() {
+int main() {   
     Стоянка гараж;
+    std::cout<<Colors::red<<"CBO ENERGY\n"<<Colors::reset;
     for (;;) {
         Машина машина;
         int inp;
-        std::cout<<"Вводи <Номер>:\n";
+        std::cout<< Colors::зачёрк <<"\nВводи <Номер>: ";
         std::cin >> inp;
+        std::cout<<Colors::reset;
         if (inp == 0) {break;}
         гараж.вставить(Машина(inp));
     }
     гараж.show();
     for (;;) {
         int id;
-        std::cout << "Введите <номер машины>: ";
+        std::cout << Colors::зачёрк << "\nВводи <номер машины>: ";
         std::cin >> id;
+        std::cout<<Colors::reset;
         if (id==0) {break;}
         Машина prim(id);
         гараж.достать(prim);
         гараж.show();
     }
+    
     return 0;
 }
